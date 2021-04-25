99e5db9b5c8293157a27cc5229504a6a525ee821
Shevkenov Andrey <shevkenov@gmail.com>
2021-04-25 09:18:16 +0300

    initial commit

---
+++ server/src/middleware/authorization.js
@@ -0,0 +1,37 @@
+import {PrismaClient} from '@prisma/client';
+import jwt from 'jsonwebtoken';
+
+const prisma = new PrismaClient()
+
+export async function getAuthUser(req, res, next) {}
+
+export async function protect(req, res, next) {
+    const token = req.headers.authorization;
+    
+    if(!token) {
+        next({
+            message: 'You need to be logged in to access this route!',
+            statusCode: 401
+        })
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await prisma.user.findUnique({
+            where: {
+                id: decode.id
+            },
+            include:{
+                videos: true
+            }
+        })
+
+        req.user = user
+        next();
+    } catch (error) {
+        next({
+            message: 'You need to be logged in to access this route!',
+            statusCode: 401
+        })
+    }
+}
---
+++ server/src/routes/auth.js
@@ -0,0 +1,61 @@
+import express from "express";
+// A function to get the routes.
+// All route definitions are in one place and we only need to export one thing
+import {PrismaClient} from '@prisma/client';
+import jwt from 'jsonwebtoken';
+
+import { protect } from '../middleware/authorization';
+
+const prisma = new PrismaClient();
+
+function getAuthRoutes() {
+  const router = express.Router();
+
+  router.post('/google-login', googleLogin);
+  router.get('/me', protect, me);
+  router.get('/signout', signout);
+
+  return router;
+}
+
+// All controllers/utility functions here
+async function googleLogin(req, res) {
+  const { username, email } = req.body
+
+  let user = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if(!user) {
+    user = await prisma.user.create({
+      data: {
+        username,
+        email
+      }
+    });
+  }
+
+  const tokenPayload = {id: user.id}
+
+  const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  })
+
+  res.cookie('token', token, { httpOnly: true });
+  res.status(200).send(token);
+}
+
+async function me(req, res) {
+  res.status(200).send({
+    user: req.user
+  })
+}
+
+function signout(req, res) {
+  res.clearCookie('token');
+  res.status(200).json({});
+}
+
+export { getAuthRoutes };
