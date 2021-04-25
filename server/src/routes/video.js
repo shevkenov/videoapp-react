993a714779d9529a4329ba74f73dfff722de5b31
Shevkenov Andrey <shevkenov@gmail.com>
2021-04-25 10:49:55 +0300

    add some controllers to video route

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -1,16 +1,105 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 function getVideoRoutes() {
   const router = express.Router();
 
+  router.get("/", getRecommendedVideos);
+  router.get("/trending", getTrendingVideos);
+  router.get("/search", searchVideos);
+
   return router;
 }
 
-async function getRecommendedVideos(req, res) {}
+async function getVideoViews(videos) {
+  for (const video of videos) {
+    const views = await prisma.view.count({
+      where: {
+        videoId: {
+          equals: video.id,
+        },
+      },
+    });
 
-async function getTrendingVideos(req, res) {}
+    video.views = views;
+  }
 
-async function searchVideos(req, res, next) {}
+  return videos;
+}
+
+async function getRecommendedVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (!videos.length) {
+    return res.status(200).json({ videos });
+  }
+
+  videos = await getVideoViews(videos);
+
+  res.status(200).json({ videos });
+}
+
+async function getTrendingVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (!videos.length) {
+    return res.status(200).json({ videos });
+  }
+
+  videos = await getVideoViews(videos);
+  videos.sort((a, b) => b.views - a.views);
+
+  res.status(200).json({ videos });
+}
+
+async function searchVideos(req, res, next) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true,
+    },
+    where: {
+      OR: [
+        {
+          title: {
+            contains: req.query.query,
+            mode: "insensitive",
+          },
+        },
+        {
+          description: {
+            contains: req.query.query,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
+
+  if (!videos.length) {
+    return res.status(200).json({ videos });
+  }
+
+  videos = await getVideoViews(videos);
+  videos.sort((a, b) => b.views - a.views);
+
+  res.status(200).json({ videos });
+}
 
 async function addVideo(req, res) {}
 
