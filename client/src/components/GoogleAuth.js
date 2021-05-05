5e62172b45e8e1f29274952b1ade7f10eaec4d50
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-05 16:47:18 +0300

    add GoogleLogin to sign in button

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Button from "../styles/Auth";
 import { SignInIcon } from "./Icons";
+import { GoogleLogin } from "react-google-login";
+import { authenticate } from '../utils/api-client';
 
 function GoogleAuth() {
+  
+
   return (
-    <Button tabIndex={0} type="button">
-      <span className="outer">
-        <span className="inner">
-          <SignInIcon />
-        </span>
-        sign in
-      </span>
-    </Button>
+    <GoogleLogin
+      render={(props) => (
+        <Button tabIndex={0} type="button" onClick={props.onClick} disabled={props.disabled}>
+          <span className="outer">
+            <span className="inner">
+              <SignInIcon />
+            </span>
+            sign in
+          </span>
+        </Button>
+      )}
+      clientId="311563765460-8amt6ikkopolg6rmq74k0otq0eseuuv0.apps.googleusercontent.com"
+      onSuccess={authenticate}
+      onFailure={authenticate}
+      cookiePolicy={'single_host_origin'}
+    />
   );
 }
 
