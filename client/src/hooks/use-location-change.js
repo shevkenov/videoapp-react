cb82a004cfa6a31a53de5b62408a1c594e044166
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-05 10:31:54 +0300

    add hook use-location-change

--- client/src/hooks/use-location-change.js
+++ client/src/hooks/use-location-change.js
@@ -1,3 +1,24 @@
-function usePrevious() {}
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 
-export function useLocationChange() {}
+function usePrevious(location) {
+    const ref = useRef();
+
+    useEffect(() => {
+        ref.current = location
+    })
+
+    return ref.current
+}
+
+export function useLocationChange(callback) {
+    const { pathname } = useLocation();
+    const prevLocation = usePrevious(pathname);
+
+    useEffect(() => {
+        if(pathname !== prevLocation) callback()
+        
+    }, [prevLocation, pathname, callback])
+    
+    return prevLocation !== pathname;
+}
