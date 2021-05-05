b6d9fe3aeae80ab7d21bb67796d59f15704d6739
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-05 10:33:24 +0300

    add toggle side bar with two hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+
+import Container from "styles/Container";
+import MobileNavbar from "components/MobileNavbar";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import Channel from "pages/Channel";
+import History from "pages/History";
+import Home from "pages/Home";
+import Library from "pages/Library";
+import LikedVideos from "pages/LikedVideos";
+import NotFound from "pages/NotFound";
+import SearchResults from "pages/SearchResults";
+import Subscriptions from "pages/Subscriptions";
+import Trending from "pages/Trending";
+import WatchVideo from "pages/WatchVideo";
+import YourVideos from "pages/YourVideos";
+
+import { useLocationChange } from './hooks/use-location-change';
 
 function App() {
-  return <>app</>;
+  const [isSideBarOpened, setSideBarOpened] = React.useState(false);
+  
+  const handleCloseSidebar = () => setSideBarOpened(false);
+  const handleToggleSidebar = () => setSideBarOpened(prevValue => !prevValue);
+  
+  useLocationChange(handleCloseSidebar);
+
+  return (
+    <>
+      <Navbar onClick={handleToggleSidebar} />
+      <Sidebar open={isSideBarOpened}/>
+      <MobileNavbar />
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/watch/:videoId" component={WatchVideo} />
+          <Route path="/channel/:channelId" component={Channel} />
+          <Route path="/results/:searchQuery" component={SearchResults} />
+          <Route path="/feed/trending" component={Trending} /> 
+          <Route path="/feed/subscriptions" component={Subscriptions} /> 
+          <Route path="/feed/library" component={Library} /> 
+          <Route path="/feed/history" component={History} /> 
+          <Route path="/feed/my_videos" component={YourVideos} /> 
+          <Route path="/feed/liked_videos" component={LikedVideos} /> 
+          <Route path="*" component={NotFound} /> 
+        </Switch>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import AppProviders from "components/AppProviders";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </Router>,
+  document.getElementById("root")
+);
