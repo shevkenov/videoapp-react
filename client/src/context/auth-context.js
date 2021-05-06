930865edfb548c64b60b465c7a7f3503f7373500
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-06 11:39:40 +0300

    add react-query

--- client/src/context/auth-context.js
+++ client/src/context/auth-context.js
@@ -1,3 +1,38 @@
-export function AuthProvider() {}
+import React, { useContext } from 'react';
+import { useQuery } from 'react-query';
 
-export function useAuth() {}
+import {axiosClient} from '../utils/api-client';
+
+const AuthContext = React.createContext(null);
+
+export function AuthProvider({children}) {
+
+    const { data } = useQuery('AuthProvider', () => axiosClient.get('/auth/me').then(res => res.data.user))
+    const user = data || null
+    
+    // const [user, setUser] = useState(null);
+
+    // useEffect(() => {
+    //     axiosClient.get('/auth/me').then(response => {
+    //         setUser(response.data.user)
+    //     }).catch(err => {
+    //         console.log(err)
+    //     })
+    // }, [])
+
+    return(
+        <AuthContext.Provider value={user}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth() {
+    const context = useContext(AuthContext);
+
+    if(context === undefined) {
+        throw new Error("Context is undefined!")
+    }
+
+    return context;
+}
