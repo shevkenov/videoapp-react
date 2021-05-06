f92fa328dc9a06272bf31b6aed682d9bc76ff8da
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-06 11:40:52 +0300

    add react-query

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+
 import App from "./App";
 import AppProviders from "components/AppProviders";
 
 ReactDOM.render(
-  <Router>
-    <AppProviders>
-      <App />
-    </AppProviders>
-  </Router>,
+  <AppProviders>
+    <App />
+  </AppProviders>,
   document.getElementById("root")
 );
--- client/src/utils/api-client.js
+++ client/src/utils/api-client.js
@@ -1,6 +1,24 @@
-export function authenticate() {}
+import axios from 'axios';
 
-export async function signoutUser() {}
+export const axiosClient = axios.create({
+    baseURL: "/api/v1"
+})
+
+export function authenticate(response) {
+    axiosClient.post('/auth/google-login', {
+        idToken: response.tokenId
+    }).then(response => {
+        console.log('Sing in success: ', response);
+        window.location.assign(window.location.href);
+    }).then(error => {
+        console.log('Sign in error: ', error)
+    })
+}
+
+export async function signoutUser() {
+    await axiosClient.get('/auth/signout');
+    window.location.pathname = "/"
+}
 
 export async function updateUser() {}
 
