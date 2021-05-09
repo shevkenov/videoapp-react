dfaa082a6af3b86ada9843098ff2c64e257b4d5c
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-09 13:58:42 +0300

    create a function that upload media in cloudinary

--- client/src/utils/upload-media.js
+++ client/src/utils/upload-media.js
@@ -1,1 +1,11 @@
-export async function uploadMedia() {}
+import axios from "axios";
+
+export async function uploadMedia({preset, file, type}) {
+    const formData = new FormData();
+    formData.append("upload_preset", preset);
+    formData.append("file", file);
+
+    const data = await axios.post(`https://api.cloudinary.com/v1_1/andrewsh/${type}/upload`, formData).then(res => res.data);
+
+    return data.secure_url;
+}
