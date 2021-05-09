e17fed4df9faa5b50547af9a7949e54114df718a
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-09 13:49:41 +0300

    map all videos

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,25 @@
+import ErrorMessage from "components/ErrorMessage";
+import VideoCard from "components/VideoCard";
 import React from "react";
+import { useQuery } from "react-query";
+import HomeSkeleton from "skeletons/HomeSkeleton";
+import { axiosClient } from "utils/api-client";
 import Wrapper from "../styles/Home";
 import VideoGrid from "../styles/VideoGrid";
 
 function Home() {
+
+  const {data: videos, isLoading, isSuccess, isError, error} = useQuery("Home", () => axiosClient.get("/videos").then(response => response.data.videos));
+
+  if(isLoading) return <HomeSkeleton />
+  if(isError) return <ErrorMessage error={error} />
+
+
   return (
     <Wrapper>
-      <VideoGrid>Recommended videos</VideoGrid>
+      <VideoGrid>
+        {isSuccess ? videos.map(video => <VideoCard video={video} key={video.id}/>) : null}
+      </VideoGrid>
     </Wrapper>
   );
 }
