4a82a1fa8f7a7006d01034a6d5b5e5934bbbfd84
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-10 18:59:06 +0300

    add useAuthAction hook which handle unauthorized users

--- client/src/hooks/use-auth-action.js
+++ client/src/hooks/use-auth-action.js
@@ -1,1 +1,21 @@
-export default function useAuthAction() {}
+import { useAuth } from "context/auth-context";
+import { useGoogleLogin } from "react-google-login";
+import { authenticate } from "utils/api-client";
+
+export default function useAuthAction() {
+    const user = useAuth();
+    const { signIn } = useGoogleLogin({
+        onSuccess: authenticate,
+        clientId:"311563765460-8amt6ikkopolg6rmq74k0otq0eseuuv0.apps.googleusercontent.com",
+    });
+
+    function handleAuthAction(authAction, data) {
+        if(user) {
+            authAction(data);
+        }else{
+            signIn()
+        }
+    }
+
+    return handleAuthAction;
+}
