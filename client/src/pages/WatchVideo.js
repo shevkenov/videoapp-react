b5ebfb10463c354008a2ff4fbbbe2b5d75e1f46f
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-10 15:44:48 +0300

    add next video commponent

--- client/src/pages/WatchVideo.js
+++ client/src/pages/WatchVideo.js
@@ -1,16 +1,27 @@
 // @ts-nocheck
 import React from "react";
+import { useQuery } from 'react-query';
+import { useParams } from "react-router";
+import { axiosClient } from "../utils/api-client";
+
 import AddComment from "../components/AddComment";
 import { DislikeIcon, LikeIcon } from "../components/Icons";
 import NoResults from "../components/NoResults";
 import VideoPlayer from "../components/VideoPlayer";
 import Button from "../styles/Button";
 import Wrapper from "../styles/WatchVideo";
+import { formatCreatedAt } from '../utils/date';
+import Skeleton from '../skeletons/WatchVideoSkeleton';
+import VideoCard from "components/VideoCard";
 
 function WatchVideo() {
-  const is404 = true;
+  const {videoId} = useParams();
+  const { data: video, isLoading: isLoadingVideo } = useQuery(["WatchVideo", videoId], () => axiosClient.get(`/videos/${videoId}`).then(res => res.data.video));
+  const { data: next, isLoading: isLoadingNext } = useQuery(["WatchVideo", "Next"], () => axiosClient.get(`/videos`).then(res => res.data.videos));
+  
+  if (isLoadingVideo || isLoadingNext) return <Skeleton />
 
-  if (is404) {
+  if (!isLoadingVideo && !isLoadingNext && !video) {
     return (
       <NoResults
         title="Page not found"
@@ -20,27 +31,27 @@ function WatchVideo() {
   }
 
   return (
-    <Wrapper filledLike={true} filledDislike={false}>
+    <Wrapper filledLike={video.isLiked} filledDislike={video.isDisliked}>
       <div className="video-container">
         <div className="video">
-          <VideoPlayer />
+          <VideoPlayer video={video}/>
         </div>
 
         <div className="video-info">
-          <h3>Title</h3>
+          <h3>{video.title}</h3>
 
           <div className="video-info-stats">
             <p>
-              <span>Views views</span> <span>•</span>{" "}
-              <span>Premiered createdAt</span>
+              <span>{video.viewsCount} views</span> <span>•</span>{" "}
+              <span>Premiered {formatCreatedAt(video.createdAt)}</span>
             </p>
 
             <div className="likes-dislikes flex-row">
               <p className="flex-row like">
-                <LikeIcon /> <span>Likes Count</span>
+                <LikeIcon /> <span>{video.likesCount}</span>
               </p>
               <p className="flex-row dislike" style={{ marginLeft: "1rem" }}>
-                <DislikeIcon /> <span>Dislikes Count</span>
+                <DislikeIcon /> <span>{video.dislikesCount}</span>
               </p>
             </div>
           </div>
@@ -51,29 +62,32 @@ function WatchVideo() {
             <div className="channel-info flex-row">
               <img
                 className="avatar md"
-                src="https://dummyimage.com/100x100"
-                alt="channel avatar"
+                src={video.user.avatar}
+                alt={video.user.username}
               />
               <div className="channel-info-meta">
-                <h4>Username</h4>
+                <h4>{video.user.username}</h4>
                 <span className="secondary small">
-                  SubscribersCount subscribers
+                  {video.subscribersCount} subscribers
                 </span>
               </div>
             </div>
 
-            <Button>Subscribe</Button>
+            {!video.isSubscribed && !video.isMain && <Button>Subscribe</Button>}
+            {video.isSubscribed && !video.isMain && <Button>Subscribed</Button>}
           </div>
 
-          <p>Description</p>
+          <p>{video.description}</p>
         </div>
 
-        <AddComment />
+        <AddComment video={video}/>
       </div>
 
       <div className="related-videos">
         <h3 className="up-next">Up Next</h3>
-        Up Next Videos
+        {
+          next.filter(v => v.id !== video.id).slice(0, 10).map(v => <VideoCard key={v.id} showAvatar={true} video={v}/>)
+        }
       </div>
     </Wrapper>
   );
