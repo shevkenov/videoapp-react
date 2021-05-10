8c9223b8f20abff5beb2851de23fb40141ddbe1d
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-10 19:00:30 +0300

    map all comments

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,27 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { formatCreatedAt } from "utils/date";
+import DeleteCommentDropdown from './DeleteCommentDropdown';
 
-function CommentList() {
-  return <>comment list</>;
+function CommentList({comments}) {
+  return (
+    <>
+      {
+        comments.length ? comments.map(comment => <Comment key={comment.id} comment={comment}/>) : null
+      }
+    </>
+  );
 }
 
-// function Comment() {
-//   return (
-//     <div className="comment">
-//       <span>
-//         <img src="" alt="avatar" />
-//       </span>
-//       <div className="comment-info" style={{ flex: "1 1 0" }}>
-//         <p className="secondary">
-//           <span>
-//             <span className="user-channel">username</span>
-//           </span>
-//           <span style={{ marginLeft: "0.6rem" }}>createdAt</span>
-//         </p>
-//         <p>Text</p>
-//       </div>
-//       <DeleteCommentDropdown />
-//     </div>
-//   );
-// }
+function Comment({comment}) {
+
+  return (
+    <div className="comment">
+      <span>
+        <Link to={`/channel/${comment.user.id}`}>
+          <img src={comment.user.avatar} alt={comment.user.username} />
+        </Link>
+      </span>
+      <div className="comment-info" style={{ flex: "1 1 0" }}>
+        <p className="secondary">
+          <span>
+            <Link to={`/channel/${comment.user.id}`}>
+              <span className="user-channel">{comment.user.username}</span>
+            </Link>
+          </span>
+          <span style={{ marginLeft: "0.6rem" }}>{formatCreatedAt(comment.createdAt)}</span>
+        </p>
+        <p>{comment.text}</p>
+      </div>
+      <DeleteCommentDropdown comment={comment}/>
+    </div>
+  );
+}
 
 export default CommentList;
