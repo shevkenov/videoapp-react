c1f3ea2e5d0b0218e895da88da945ca51848e179
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-18 15:19:29 +0300

    feat: add tab layout to myChannels page

--- client/src/components/ChannelTabChannels.js
+++ client/src/components/ChannelTabChannels.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Wrapper from "../styles/ChannelTabChannels";
 
-function ChannelTabChannels() {
-  return <Wrapper>Channel Channels</Wrapper>;
+function ChannelTabChannels({ channels }) {
+  if (!channels.length) {
+    return <p>Not subscribed any channels yet?</p>;
+  }
+  return (
+    <Wrapper>
+      {channels
+        ? channels.map((channel) => (
+            <Link key={channel.id} to={`/channel/${channel.id}`}>
+              <div className="channel">
+                <img src={channel.avatar} alt={channel.username} />
+                <h3>{channel.username}</h3>
+                <p className="secondary">
+                  {channel.subscribersCount} subscribers
+                </p>
+              </div>
+            </Link>
+          ))
+        : null}
+    </Wrapper>
+  );
 }
 
 export default ChannelTabChannels;
--- client/src/components/ChannelTabVideo.js
+++ client/src/components/ChannelTabVideo.js
@@ -1,8 +1,21 @@
 import React from "react";
 import Wrapper from "../styles/ChannelTabVideo";
+import VideoCard from "./VideoCard";
 
-function ChannelTabVideo() {
-  return <Wrapper>Channel Videos</Wrapper>;
+function ChannelTabVideo({ videos }) {
+  if (!videos.length) {
+    return <p>This channel hasn't posted any videos yet</p>;
+  }
+
+  return (
+    <Wrapper>
+      <div className="videos">
+        {videos.map((video) => (
+          <VideoCard key={video.id} video={video} showAvatar noUsername />
+        ))}
+      </div>
+    </Wrapper>
+  );
 }
 
 export default ChannelTabVideo;
