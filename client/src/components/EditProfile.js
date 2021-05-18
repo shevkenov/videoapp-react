4781550420497cdf8d773f0160af5f86dcf05bbd
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-18 20:20:52 +0300

    feat: add functionality to edit profile

--- client/src/components/EditProfile.js
+++ client/src/components/EditProfile.js
@@ -2,15 +2,23 @@
 import React from "react";
 import Button from "../styles/Button";
 import Wrapper from "../styles/EditProfile";
+import EditProfileModal from "./EditProfileModal";
 
-function EditProfile() {
+function EditProfile({ profile }) {
+  const [showProfileModal, setShowProfileModal] = React.useState(false);
+  
+  const closeModal = () => setShowProfileModal(false);
+
   return (
     <>
       <Wrapper>
         <div>
-          <Button grey>Edit Profile</Button>
+          <Button grey onClick={() => setShowProfileModal(true)}>Edit Profile</Button>
         </div>
       </Wrapper>
+      {
+        showProfileModal ? <EditProfileModal profile={profile} closeModal={closeModal} /> : null
+      }
     </>
   );
 }
--- client/src/components/EditProfileModal.js
+++ client/src/components/EditProfileModal.js
@@ -1,17 +1,65 @@
 import React from "react";
+import { useSnackbar } from "react-simple-snackbar";
+import { updateUser } from "utils/api-client";
+import { uploadMedia } from "utils/upload-media";
 import Button from "../styles/Button";
 import Wrapper from "../styles/EditProfileModal";
 import { CloseIcon } from "./Icons";
 
-function EditProfileModal() {
+function EditProfileModal({profile, closeModal}) {
+  const [avatar, setAvatar] = React.useState(profile.avatar);
+  const [cover, setCover] = React.useState(profile.cover);
+  const [openSnackBar] = useSnackbar()
+  
+  const handleSave = async (e) => {
+    e.preventDefault();
+    const user = {
+      username: e.target.elements.username.value,
+      about: e.target.elements.about.value,
+      avatar,
+      cover
+    }
+
+    if(!user.username.trim()) return openSnackBar("Username must persist")
+
+
+    await updateUser(user);
+    openSnackBar("Profile updated!");
+    closeModal();
+  }
+
+  const handleChangeCover = async(e) => {
+    const file = e.target.files[0];
+
+    const url = await uploadMedia({
+      preset: "xxe4qgrv",
+      file,
+      type: "image"
+    })
+
+    setCover(url);
+  } 
+
+  const handleChangeAvatar = async(e) => {
+    const file = e.target.files[0];
+
+    const url = await uploadMedia({
+      preset: "rgsx3lof",
+      file,
+      type: "image"
+    })
+
+    setAvatar(url);
+  }
+  
   return (
     <Wrapper>
       <div className="container"></div>
       <div className="edit-profile">
-        <form>
+        <form onSubmit={handleSave}>
           <div className="modal-header">
             <h3>
-              <CloseIcon />
+              <CloseIcon onClick={closeModal}/>
               <span>Edit Profile</span>
             </h3>
             <Button type="submit">Save</Button>
@@ -23,8 +71,8 @@ function EditProfileModal() {
                 className="pointer"
                 width="100%"
                 height="200px"
-                src="https://dummyimage.com/600x200"
-                alt="cover"
+                src={profile.cover}
+                alt={profile.username}
               />
             </label>
             <input
@@ -32,15 +80,16 @@ function EditProfileModal() {
               type="file"
               accept="image/*"
               style={{ display: "none" }}
+              onChange={handleChangeCover}
             />
           </div>
 
           <div className="avatar-upload-icon">
             <label htmlFor="avatar-upload">
               <img
-                src="https://dummyimage.com/250x250"
+                src={profile.avatar}
                 className="pointer avatar lg"
-                alt="avatar"
+                alt={profile.username}
               />
             </label>
             <input
@@ -48,15 +97,15 @@ function EditProfileModal() {
               type="file"
               accept="image/*"
               style={{ display: "none" }}
+              onChange={handleChangeAvatar}
             />
           </div>
           <input
             type="text"
             placeholder="Insert username"
             id="username"
-            required
           />
-          <textarea id="about" placeholder="Tell viewers about your channel" />
+          <textarea id="about" placeholder="Tell viewers about your channel"/>
         </form>
       </div>
     </Wrapper>
