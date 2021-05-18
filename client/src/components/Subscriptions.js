68270fe40c2ab6e6f0631203e5d154f6bcb152a3
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-18 08:56:20 +0300

    fix: small typos

--- client/src/components/Subscriptions.js
+++ client/src/components/Subscriptions.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Wrapper from "../styles/Subscriptions";
 
+
 function Subscriptions({user}) {
   if(!user) return null;
   
