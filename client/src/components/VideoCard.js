0e165bfc4c3e7d325b2bde27fd1835cd10694e56
Shevkenov Andrey <shevkenov@gmail.com>
2021-05-18 15:18:04 +0300

    fix: add noUsername and showAvatar props

--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -5,35 +5,36 @@ import Avatar from "../styles/Avatar";
 import Wrapper from "../styles/VideoCard";
 import DeleteVideoDropdown from "./DeleteVideoDropdown";
 
-function VideoCard({video, showAvatar}) {
+function VideoCard({ video, showAvatar, noUsername }) {
   return (
     <Wrapper>
       <Link to={`/watch/${video.id}`}>
-        <img
-          className="thumb"
-          src={video.thumbnail}
-          alt={video.title}
-        />
+        <img className="thumb" src={video.thumbnail} alt={video.title} />
       </Link>
       <div className="video-info-container">
-        {
-          !showAvatar && (<div className="channel-avatar">
-          <Avatar
-            style={{ marginRight: "0.8rem" }}
-            src={video.user.avatar}
-            alt={video.user.username}
-          />
-        </div>)
-        }
+        {!showAvatar && (
+          <div className="channel-avatar">
+            <Avatar
+              style={{ marginRight: "0.8rem" }}
+              src={video.user.avatar}
+              alt={video.user.username}
+            />
+          </div>
+        )}
         <div className="video-info">
           <span>
             <h4 className="truncate">{video.title}</h4>
           </span>
-          <span>
-            <span className="secondary">{video.user.username}</span>
-          </span>
+          {!noUsername && (
+            <Link to={`/channel/${video.user.id}`}>
+              <span>
+                <span className="secondary">{video.user.username}</span>
+              </span>
+            </Link>
+          )}
           <p className="secondary leading-4">
-            <span>{video.views} views</span> <span>•</span> <span>{formatCreatedAt(video.createdAt)}</span>
+            <span>{video.views} views</span> <span>•</span>{" "}
+            <span>{formatCreatedAt(video.createdAt)}</span>
           </p>
         </div>
         <DeleteVideoDropdown />
